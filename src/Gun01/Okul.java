4afdf3526172a3e22624ac95a21427718e8b63f2
fatih <fatihhan.karahanli@gmail.com>
2023-08-03 19:33:19 +0300

    Üçüncü taskımı da bitirdim

--- src/Gun01/Okul.java
+++ src/Gun01/Okul.java
@@ -3,6 +3,7 @@ package Gun01;
 public class Okul {
     public static void main(String[] args) {
         Ogrenci ogr1 = new Ogrenci();
-        ogr1.ad = "İsmet";
+
+        ogr1.soyad = "Temur";
     }
 }
